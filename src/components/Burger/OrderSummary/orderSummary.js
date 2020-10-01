109966ed7dbac3f7ffb8030dadbb648b62703259
kookie09 <62861248+kookie09@users.noreply.github.com>
2020-10-02 09:58:13 +1000

    Adding order Summary modal

--- src/components/Burger/OrderSummary/orderSummary.js
+++ src/components/Burger/OrderSummary/orderSummary.js
@@ -3,9 +3,21 @@ import React from 'react';
 import Aux from '../../../hoc/Auxiliary';
 
 const orderSummary = (props) => {
+    const ingredientSummary = Object.keys(props.ingredients)
+        .map(igKey => {
+            return <li>
+                <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {props.ingredients[igKey]}
+            </li>
+        });
+
     return (
         <Aux>
             <h3>Your Order</h3>
+            <p>Your burger has following ingredients: </p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+            <p>Continue to Checkout ?</p>
         </Aux>
     );
 };
