e96440de25d97693feebaa34b7859fa52b710c0a
kookie09 <62861248+kookie09@users.noreply.github.com>
2020-10-02 11:42:17 +1000

    add backdrop

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -23,7 +23,8 @@ class BurgerBuilder extends Component {
             meat: 0
         },
         totalPrice: 4,
-        purchaseable: false
+        purchaseable: false,
+        purchasing: false
     }
 
     updatePurchaseState = (ingredients) => {
@@ -79,6 +80,13 @@ class BurgerBuilder extends Component {
         this.updatePurchaseState(updatedIngredients);
     };
 
+    purchaseHandler = () => {
+        this.setState({purchasing: true});
+    }
+
+    purchaseCancelHandler = () => {
+        this.setState({purchasing: false});
+    }
 
 
     render() {
@@ -90,7 +98,9 @@ class BurgerBuilder extends Component {
         }
         return (
             <Aux>
-                <Modal>
+                <Modal 
+                    show = {this.state.purchasing}
+                    modalClosed = {this.purchaseCancelHandler}>
                     <OrderSummary ingredients = {this.state.ingredients}/>
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
@@ -100,6 +110,7 @@ class BurgerBuilder extends Component {
                     disabled={disbabledInfo}
                     price={this.state.totalPrice}
                     purchaseable={this.state.purchaseable}
+                    ordered = {this.purchaseHandler}
                 />
             </Aux>
         );
