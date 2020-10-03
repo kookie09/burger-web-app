0b08a603368516d62445e6597b815fea8f4aaab6
kookie09 <62861248+kookie09@users.noreply.github.com>
2020-10-03 10:16:37 +1000

    add continue and cancel button to modal

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -88,6 +88,10 @@ class BurgerBuilder extends Component {
         this.setState({purchasing: false});
     }
 
+    purchaseContinueHandler = () => {
+        alert("you continue");
+    }
+
 
     render() {
         const disbabledInfo = {
@@ -101,7 +105,11 @@ class BurgerBuilder extends Component {
                 <Modal 
                     show = {this.state.purchasing}
                     modalClosed = {this.purchaseCancelHandler}>
-                    <OrderSummary ingredients = {this.state.ingredients}/>
+                    <OrderSummary 
+                        ingredients = {this.state.ingredients}
+                        purchaseCancelled = {this.purchaseCancelHandler}
+                        purchaseContinued = {this.purchaseContinueHandler}
+                    />
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls
