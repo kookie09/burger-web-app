4e400dc98b739981fcdeb958b8aab3e3ddc44a8a
kookie09 <62861248+kookie09@users.noreply.github.com>
2020-10-03 23:35:55 +1000

    tweak sidedraw to show conditionally

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../hoc/Auxiliary';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar />
-        <SideDrawer />
+class Layout extends Component {
+    
+    state = {
+        showSideDrawer: false
+    }
+
+    SideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false});
+    }
+
+    sideDrawerToggleHandler = () => {
+    
+        this.setState((prevState) => {
+          return { showSideDrawer: !prevState.showSideDrawer };
+    });
+
+    }
+    
+    render () {
+        return (
+<Aux>
+        <Toolbar drawerToggleClicked = {this.sideDrawerToggleHandler}/>
+        <SideDrawer open = {this.state.showSideDrawer} closed = {this.SideDrawerClosedHandler}/>
         <main className = {classes.Content}>
-            {props.children}
+            {this.props.children}
         </main>
     </Aux>
-);
-
-export default layout;
+        );
+    }
+} 
+export default Layout;
