57d37cace9aea30f9257ad09689ba3c463f9297a
kookie09 <62861248+kookie09@users.noreply.github.com>
2020-10-17 22:45:51 +1100

    forwarding authenticed users

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import {Redirect} from 'react-router-dom';
 
 import Button from '../../components/UI/Button/Button';
 import Input from '../../components/UI/Input/Input';
@@ -132,8 +133,15 @@ class Auth extends Component {
             );
         }
 
+        let authRedirect = null;
+
+        if(this.props.isAuthenticated) {
+            authRedirect = <Redirect to = "/"/>
+        }
+
         return (
             <div className={classes.Auth}>
+                {authRedirect}
                 {errorMessage
                 }
                 <form onSubmit={this.onSubmitHandler}>
@@ -153,7 +161,8 @@ class Auth extends Component {
 const mapStateToProps = state => {
     return {
         loading: state.auth.loading,
-        error: state.auth.error
+        error: state.auth.error,
+        isAuthenticated: state.auth.token !== null
     };
 };
 
