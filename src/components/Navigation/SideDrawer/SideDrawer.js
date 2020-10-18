70458581e80a54f2c7e369f7b7a9f45db70adde5
kookie09 <62861248+kookie09@users.noreply.github.com>
2020-10-18 14:06:23 +1100

    fix front pages and UI

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -10,17 +10,19 @@ const sideDrawer = (props) => {
 
     let attachedClasses = [classes.SideDrawer, classes.Close];
 
-    if(props.open) {
-        attachedClasses =  [classes.SideDrawer, classes.Open]
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open]
     }
 
     return (
         <Aux>
-            <Backdrop show = {props.open} clicked = {props.closed}/>
-            <div className={attachedClasses.join(" ")}>
+            <Backdrop show={props.open} clicked={props.closed} />
+            <div 
+                className={attachedClasses.join(" ")}
+                onClick = {props.closed}>
                 <div className={classes.Logo}><Logo /></div>
                 <nav>
-                    <NagivationItems isAuthenticated = {props.isAuth}/>
+                    <NagivationItems isAuthenticated={props.isAuth} />
                 </nav>
             </div>
         </Aux>
